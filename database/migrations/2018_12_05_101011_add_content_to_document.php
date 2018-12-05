f4b5bc3891cc51f9831163a4cf44bb58ce10e5b6
Chloé Corfmat <chloe.corfmat@capgemini.com>
2018-12-05 11:29:28 +0100

    Update import for others parts and update database for documents table

---
+++ database/migrations/2018_12_05_101011_add_content_to_document.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddContentToDocument extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::statement('ALTER TABLE documents MODIFY url VARCHAR(255) null');
+
+        Schema::table('documents', function (Blueprint $table) {
+            $table->longText('content')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('documents', function (Blueprint $table) {
+            //
+        });
+    }
+}
