754aa542aa4c7d417467539e73f5d9db47e65304
Chloé Corfmat <chloe.corfmat@capgemini.com>
2019-08-28 16:57:44 +0200

    Update badge seeder

--- database/seeds/BadgeSeeder.php
+++ database/seeds/BadgeSeeder.php
@@ -17,35 +17,35 @@ class BadgeSeeder extends Seeder
         Badge::create([
             'badge_type_id' => $badge_type_id,
             'level_id' => 1,
-            'image' => '',
+            'image' => './badges/mountain-peak-1.svg',
             'nb_repetitions' => 5
         ]);
 
         Badge::create([
             'badge_type_id' => $badge_type_id,
             'level_id' => 2,
-            'image' => '',
+            'image' => './badges/mountain-peak-2.svg',
             'nb_repetitions' => 25
         ]);
 
         Badge::create([
             'badge_type_id' => $badge_type_id,
             'level_id' => 3,
-            'image' => '',
+            'image' => './badges/mountain-peak-3.svg',
             'nb_repetitions' => 50
         ]);
 
         Badge::create([
             'badge_type_id' => $badge_type_id,
             'level_id' => 4,
-            'image' => '',
+            'image' => './badges/mountain-peak-4.svg',
             'nb_repetitions' => 100
         ]);
 
         Badge::create([
             'badge_type_id' => $badge_type_id,
             'level_id' => 5,
-            'image' => '',
+            'image' => './badges/mountain-peak-5.svg',
             'nb_repetitions' => 365
         ]);
 
@@ -53,35 +53,35 @@ class BadgeSeeder extends Seeder
         Badge::create([
         'badge_type_id' => $badge_type_id,
         'level_id' => 1,
-        'image' => '',
+        'image' => './badges/mountain-peak-1.svg',
         'nb_repetitions' => 10
         ]);
 
         Badge::create([
             'badge_type_id' => $badge_type_id,
             'level_id' => 2,
-            'image' => '',
+            'image' => './badges/mountain-peak-2.svg',
             'nb_repetitions' => 25
         ]);
 
         Badge::create([
             'badge_type_id' => $badge_type_id,
             'level_id' => 3,
-            'image' => '',
+            'image' => './badges/mountain-peak-3.svg',
             'nb_repetitions' => 50
         ]);
 
         Badge::create([
             'badge_type_id' => $badge_type_id,
             'level_id' => 4,
-            'image' => '',
+            'image' => './badges/mountain-peak-4.svg',
             'nb_repetitions' => 100
         ]);
 
         Badge::create([
             'badge_type_id' => $badge_type_id,
             'level_id' => 5,
-            'image' => '',
+            'image' => './badges/mountain-peak-5.svg',
             'nb_repetitions' => 300
         ]);
 
