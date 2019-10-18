c31a45d8e9231bfd5b388ec948fb116ff80bdc24
Chloé Corfmat <chloe.corfmat@kleegroup.com>
2019-10-18 15:10:51 +0200

    Add seeder for badges and badge_types

--- database/seeds/BadgeSeeder.php
+++ database/seeds/BadgeSeeder.php
@@ -156,5 +156,149 @@ class BadgeSeeder extends Seeder
             'image' => './badges/scoreboard-5.svg',
             'nb_repetitions' => 1000
         ]);**/
+
+        $badge_type_id = BadgeType::where('method', 'validateCompositeTestsXTimes')->first()->id;
+        Badge::create([
+            'badge_type_id' => $badge_type_id,
+            'level_id' => 1,
+            'image' => './badges/scoreboard-1.svg',
+            'nb_repetitions' => 2
+        ]);
+
+        Badge::create([
+            'badge_type_id' => $badge_type_id,
+            'level_id' => 2,
+            'image' => './badges/scoreboard-2.svg',
+            'nb_repetitions' => 5
+        ]);
+
+        Badge::create([
+            'badge_type_id' => $badge_type_id,
+            'level_id' => 3,
+            'image' => './badges/scoreboard-3.svg',
+            'nb_repetitions' => 10
+        ]);
+
+        Badge::create([
+            'badge_type_id' => $badge_type_id,
+            'level_id' => 4,
+            'image' => './badges/scoreboard-4.svg',
+            'nb_repetitions' => 25
+        ]);
+
+        Badge::create([
+            'badge_type_id' => $badge_type_id,
+            'level_id' => 5,
+            'image' => './badges/scoreboard-5.svg',
+            'nb_repetitions' => 50
+        ]);
+
+        $badge_type_id = BadgeType::where('method', 'comeXTimesInChallengeTop')->first()->id;
+        Badge::create([
+            'badge_type_id' => $badge_type_id,
+            'level_id' => 1,
+            'image' => './badges/scoreboard-1.svg',
+            'nb_repetitions' => 1
+        ]);
+
+        Badge::create([
+            'badge_type_id' => $badge_type_id,
+            'level_id' => 2,
+            'image' => './badges/scoreboard-2.svg',
+            'nb_repetitions' => 3
+        ]);
+
+        Badge::create([
+            'badge_type_id' => $badge_type_id,
+            'level_id' => 3,
+            'image' => './badges/scoreboard-3.svg',
+            'nb_repetitions' => 5
+        ]);
+
+        Badge::create([
+            'badge_type_id' => $badge_type_id,
+            'level_id' => 4,
+            'image' => './badges/scoreboard-4.svg',
+            'nb_repetitions' => 7
+        ]);
+
+        Badge::create([
+            'badge_type_id' => $badge_type_id,
+            'level_id' => 5,
+            'image' => './badges/scoreboard-5.svg',
+            'nb_repetitions' => 11
+        ]);
+
+        $badge_type_id = BadgeType::where('method', 'beFirstXTimesOnLesson')->first()->id;
+        Badge::create([
+            'badge_type_id' => $badge_type_id,
+            'level_id' => 1,
+            'image' => './badges/scoreboard-1.svg',
+            'nb_repetitions' => 1
+        ]);
+
+        Badge::create([
+            'badge_type_id' => $badge_type_id,
+            'level_id' => 2,
+            'image' => './badges/scoreboard-2.svg',
+            'nb_repetitions' => 3
+        ]);
+
+        Badge::create([
+            'badge_type_id' => $badge_type_id,
+            'level_id' => 3,
+            'image' => './badges/scoreboard-3.svg',
+            'nb_repetitions' => 5
+        ]);
+
+        Badge::create([
+            'badge_type_id' => $badge_type_id,
+            'level_id' => 4,
+            'image' => './badges/scoreboard-4.svg',
+            'nb_repetitions' => 7
+        ]);
+
+        Badge::create([
+            'badge_type_id' => $badge_type_id,
+            'level_id' => 5,
+            'image' => './badges/scoreboard-5.svg',
+            'nb_repetitions' => 11
+        ]);
+
+        $badge_type_id = BadgeType::where('method', 'getXPointsInExercises')->first()->id;
+        Badge::create([
+            'badge_type_id' => $badge_type_id,
+            'level_id' => 1,
+            'image' => './badges/scoreboard-1.svg',
+            'nb_repetitions' => 200
+        ]);
+
+        Badge::create([
+            'badge_type_id' => $badge_type_id,
+            'level_id' => 2,
+            'image' => './badges/scoreboard-2.svg',
+            'nb_repetitions' => 500
+        ]);
+
+        Badge::create([
+            'badge_type_id' => $badge_type_id,
+            'level_id' => 3,
+            'image' => './badges/scoreboard-3.svg',
+            'nb_repetitions' => 1000
+        ]);
+
+        Badge::create([
+            'badge_type_id' => $badge_type_id,
+            'level_id' => 4,
+            'image' => './badges/scoreboard-4.svg',
+            'nb_repetitions' => 5000
+        ]);
+
+        Badge::create([
+            'badge_type_id' => $badge_type_id,
+            'level_id' => 5,
+            'image' => './badges/scoreboard-5.svg',
+            'nb_repetitions' => 10000
+        ]);
     }
 }
--- database/seeds/BadgeTypeSeeder.php
+++ database/seeds/BadgeTypeSeeder.php
@@ -35,5 +35,32 @@ class BadgeTypeSeeder extends Seeder
             'description' => 'X dépend du niveau',
             'method' => 'getATotalOfXPointsInChallenge'
         ]);**/
+
+        BadgeType::create([
+            'name' => 'Valider X fois un test composé',
+            'description' => 'X dépend du niveau',
+            'method' => 'validateCompositeTestsXTimes'
+        ]);
+
+        BadgeType::create([
+            'name' => 'Atteindre le top 10 des challenges X fois',
+            'description' => 'X dépend du niveau',
+            'method' => 'comeXTimesInChallengeTop'
+        ]);
+
+        BadgeType::create([
+            'name' => 'Majorer une leçon X fois',
+            'description' => 'X dépend du niveau',
+            'method' => 'beFirstXTimesOnLesson'
+        ]);
+
+        BadgeType::create([
+            'name' => 'Donner X bonnes réponses dans les exercices et tests composés (tous confondus)',
+            'description' => 'X dépend du niveau',
+            'method' => 'getXPointsInExercises'
+        ]);
+
+
+
     }
 }
